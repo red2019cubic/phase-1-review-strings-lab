e3b798ea624dc7d0844a8ae328494404d163fd55
Red <red.sarouf2014@gmail.com>
2023-03-06 09:34:02 -0500

    Added Strings Lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = currentUser + 'Welcome to Flatbook, ' + "!";
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = "Welcome, " + currentUser.slice(0,5) + "!";
